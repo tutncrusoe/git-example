77b054f369cebb41ab92bc6644b131228a534f71
tutncrusoe <huydinhanhle@gmail.com>
2023-03-11 12:34:23 +0700

    create project

---
+++ src/test/java/com/example/gitexample/GitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
